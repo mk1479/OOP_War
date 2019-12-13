2d6dac4badf490531e8d04b86d87c87df60ef3ea
jackly5154 <49957081+jackly5154@users.noreply.github.com>
2019-12-12 19:03:25 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,10 +1,13 @@
+
 /** ADD A COMPARISON CLASS TO COMPARE CARDS <- moved to Deck class
 * ADD A LIST OF THE RANKS OF VALUES USING A MAP?? - 
 * potentially easier than the try catch but maybe not necessary??
 * ADD A TOSTRING METHOD TO PRINT CARD - maybe not bc all strings, use get methods
 */
+import java.util.*;
+import java.util.Map;
 
-public abstract class Card{
+public class Card{
 
 	protected String suit;
 	protected int rank;
@@ -12,7 +15,7 @@ public abstract class Card{
 	protected String color; //Changed from char to string to keep it modular
 
 
-	public Card(int rank){
+	public Card(int val){
 		rank = val;
 		suit = color = fvalue = "";
 	}
@@ -24,15 +27,16 @@ public abstract class Card{
 		try{ rank = Integer.parseInt(val);}
 		catch(Exception e) {
 			//Set A to 14? since it beats all other cards
-			if(val.toUpperCase() == "A")
+			if((val.toUpperCase()).equals("A"))
 				rank = 14;
-			else if(val.toUpperCase() == "J")
+			else if((val.toUpperCase()).equals("J"))
 				rank = 11;
-			else if(val.toUpperCase() == "Q")
+			else if((val.toUpperCase()).equals("Q"))
 				rank = 12;
-			else if(val.toUpperCase() == "K")
+			else if((val.toUpperCase()).equals("K"))
 				rank = 13;
 		}
+		
 	}
 
 	public String getFValue() {
@@ -49,6 +53,10 @@ public abstract class Card{
 
 	public int getRank() {
 		return this.rank;
+	}
+	
+	public String toString() {
+		return fvalue + " of " + suit;
 	}
 
 
