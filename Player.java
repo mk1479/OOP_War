89d345ef9456e268ee2d944b4d18abcea57338d1
jackly5154 <49957081+jackly5154@users.noreply.github.com>
2019-12-12 19:01:55 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,24 +1,25 @@
-/* ADDED SETPLACE FUNCTION
-* ADDED GETROUNDSWON FUNCTION
-*/
 
 
-public abstract class Player{
+//import java.util.ArrayList;
+
+
+public class Player{
 
 	protected String name;
 	protected Deck deck;
 	protected int place;
-	protected int roundsWon;
+	//protected int roundsWon;
 
 	public Player(String pName, Deck pDeck){
 		name = pName;
 		deck = pDeck;
-		roundsWon = place = 0;
+		//roundsWon = place = 0;
 	}
 	
-	public void addCards(ArrayList<Card> c){
-		for(int i = 0; i < c.size(); i++)
-			(this.deck).add(c[i]);
+	//CHECK IF ARRAYLIST OR DECK AS PARAMETER
+	public void addCards(Deck c){
+		for(int i = 0; i < c.getSize(); i++)
+			(this.deck).add(c.getCard(i));
 	}
 
 	public Card playCard(){
@@ -28,14 +29,26 @@ public abstract class Player{
 	public int getPlace(){
 		return this.place;
 	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public boolean isEmpty() {
+		return deck.isEmpty();
+	}
+	
+	public int getDSize() {
+		return deck.getSize();
+	}
 
-	public void incRoundsWon(){
+	/*public void incRoundsWon(){
 		roundsWon++;
 	}
 
 	public int getRoundsWon(){
 		return this.roundsWon;
-	}
+	}*/
 
 	public void setPlace(int p){
 		this.place = p;
